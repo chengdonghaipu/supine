7c5b63241e678bbb59cb992775260f5cca997f10
chendong <1151849952@qq.com>
2020-10-12 23:02:20 +0800

    优化模型模板

--- projects/dev/src/app/form.model.ts
+++ projects/dev/src/app/form.model.ts
@@ -21,9 +21,14 @@ export class FormModel extends BaseFormModel {
   @InputModel<FormModel>({label: '矿岩名称4', areaId: 5})
   mineralName3 = [3];
 
-  modelUpdateHook(formValue: any, model: FormControlConfig[], ...params: any[]): void {
-    super.modelUpdateHook(formValue, model, ...params);
-    console.log(model);
-    // return model.filter((value, index) => index > 2);
+  /**
+   * 更新表单模型钩子
+   * @param formValue 当表单初始化后 formValue就为表单对象的value 否则为null
+   * @param model 注册了的模型配置数组 可以根据某些条件进行过滤 来动态控制表单
+   * @param params 调用 executeModelUpdate方法传的参数 以此来更加灵活来动态控制表单
+   * @return 如果返回值为void 则渲染所有注册的表单控件 如果返回表单控件数组 则只渲染该数组中的控件模型
+   */
+  modelUpdateHook(formValue: any, model: FormControlConfig[], ...params: any[]): FormControlConfig[] | void {
+    return model;
   }
 }
