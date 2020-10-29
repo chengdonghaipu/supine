d95ef44b24c5524d0df4bb67c4b285d635dc7fb9
Chen Jiadong <1151849952@qq.com>
2020-10-29 16:20:42 +0800

    发布10.0.0-rc22 表单模型支持updateValueAndValidity返回rxjs流

---
+++ projects/http/src/lib/http.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-http',
+  template: `
+    <p>
+      http works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HttpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
